d4c077467b559d488808aab4e859e6736a962eb6
Hailey <Hailey@Hailey-LT-W10>
2021-03-27 14:18:59 +0900

    Test

---
+++ Day20210318/src/kr/co/kh/HelloKh.java
@@ -0,0 +1,22 @@
+package kr.co.kh;
+/*작성자: 정혜원
+ * 작성일: 2021.3.18
+ */
+public class HelloKh {
+	public static void main(String[] args) {
+		//콘솔에 출력하는 실행문
+		/*
+		int x=1; //복사(=)입니다 (int)정수(+,-,0) 
+		int y=2;
+		int result = x+y;
+		System.out.println("x와y의 더한 값은="+result);
+				//컴퓨터에서 모니터에서 출력하라(줄을 넘겨라)
+		System.out.println("======");
+		*/
+		int x=1; int y=2;
+		int result = 
+				x+y;
+		System.out.println(result);
+		
+	}
+}
---
+++ Day20210318/src/kr/co/kh/HelloWorld.java
@@ -0,0 +1,10 @@
+package kr.co.kh;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("자바수업");
+
+	}
+
+}
